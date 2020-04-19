1ffe9a77919ebaea27c7d893e1a22021bbe8ef46
Aboy <e.aboy@sinteac.es>
2020-04-19 20:31:06 +0200

    Add inputs and output to operation component

--- src/app/components/operation/operation.component.ts
+++ src/app/components/operation/operation.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-operation',
   templateUrl: './operation.component.html',
   styleUrls: ['./operation.component.scss']
 })
-export class OperationComponent implements OnInit {
+export class OperationComponent {
+
+  @Input() firstNumber: number;
+  @Input() secondNumber: number;
+  @Input() result: number = null;
+  @Input() operationSign: string;
+  @Output() solved = new EventEmitter<number>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  solve() {
+    this.solved.emit(this.result);
   }
 
 }
