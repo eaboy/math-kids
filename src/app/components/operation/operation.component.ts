1adc3b53a491b12fc6be05be6ed1fd3a8fdd7982
Aboy <e.aboy@sinteac.es>
2020-04-26 20:16:33 +0200

    Visual progress to new operation

--- src/app/components/operation/operation.component.ts
+++ src/app/components/operation/operation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { Operation, OperationResult } from 'src/app/operations.service';
 
 @Component({
@@ -6,15 +6,29 @@ import { Operation, OperationResult } from 'src/app/operations.service';
   templateUrl: './operation.component.html',
   styleUrls: ['./operation.component.scss']
 })
-export class OperationComponent {
+export class OperationComponent implements OnChanges {
 
   @Input() operation: Operation;
   @Input() result: number | null;
   @Input() showIcon: boolean;
   @Input() correct: boolean;
+  @Input() secondsToNewOperation: number;
   @Output() solved = new EventEmitter<OperationResult>();
+  overlayWidth = 0;
 
   constructor() { }
+
+  ngOnChanges(change: SimpleChanges) {
+    if (change.correct?.currentValue) {
+      const interval = setInterval(() => {
+        this.overlayWidth++;
+        if (this.overlayWidth === 100) {
+          clearInterval(interval);
+          this.overlayWidth = 0;
+        }
+      }, this.secondsToNewOperation / 100 * 1000);
+    }
+  }
 
   solve() {
     const operationResult: OperationResult = { ...this.operation, result: this.result };
