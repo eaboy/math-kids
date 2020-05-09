30ef311fd81909e65c93adc83857bd7db4d6bfda
Aboy <e.aboy@sinteac.es>
2020-05-09 21:23:06 +0200

    Added validators to config form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { OperationType } from '../operations.service';
 import { StatusService, OperationsPageConfiguration } from '../status.service';
 import { Router } from '@angular/router';
@@ -14,8 +14,8 @@ export class HomeComponent {
   operationTypesArray = Object.entries(OperationType).map(([name, value]) => ({ name, value }));
 
   operationConfig = new FormGroup({
-    secondsToNewOperation: new FormControl(3),
-    totalOperations: new FormControl(10),
+    secondsToNewOperation: new FormControl(3, [Validators.min(1)]),
+    totalOperations: new FormControl(10, [Validators.min(0)]),
     operationType: new FormControl(OperationType.addition)
   });
 
