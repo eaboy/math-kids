d673e1302cf6e27d7d132aded194d9e38901621f
Aboy <e.aboy@sinteac.es>
2020-05-09 20:11:56 +0200

    Added link to navigate to home when finished operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from './components/components.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    ComponentsModule
+    ComponentsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,13 +4,15 @@ import { OperationComponent } from './operation/operation.component';
 import { NumpadComponent } from './numpad/numpad.component';
 import { VisualHelpComponent } from './visual-help/visual-help.component';
 import { SuccessComponent } from './success/success.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [OperationComponent, NumpadComponent, VisualHelpComponent, SuccessComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     OperationComponent,
